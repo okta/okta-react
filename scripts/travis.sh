ab6d018a697e75cafe5ce3432c1e002b63a54c35
Vijet M <vijet.mahabaleshwar@okta.com>
2020-11-03 18:44:18 +0000

    chore: Travis ci fix
    OKTA-343265
    <<<Jenkins Check-In of Tested SHA: b524d476585d964a14c0ebc02a62ac0a7f04cdc8 for eng_productivity_ci_bot_okta@okta.com>>>
    Artifact: okta-react

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -1,6 +1,7 @@
 set -e
 # if this build was triggered via a cron job, run tests on sauce labs
 if [ "${TRAVIS_EVENT_TYPE}" = "cron" ] ; then
+	yarn pretest
     yarn test:e2e
 else
     # run the lint, unit and e2e tests (on chrome headless)
