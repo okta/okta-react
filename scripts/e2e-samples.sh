092b25a8777aef71e03ce7308cd9b026535759bb
Shuo Wu <wushuo2010@gmail.com>
2021-07-30 08:56:06 -0400

    update e2e-samples script

--- scripts/e2e-samples.sh
+++ scripts/e2e-samples.sh
@@ -2,18 +2,22 @@
 
 source ${OKTA_HOME}/${REPO}/scripts/setup.sh
 
-setup_service google-chrome-stable 83.0.4103.61-1
+setup_service xvfb start
+setup_service java 1.8.222
+setup_service google-chrome-stable 87.0.4280.66-1
 
 export TEST_SUITE_TYPE="junit"
 export TEST_RESULT_FILE_DIR="${REPO}/test-reports/e2e"
 
+export BROWSER=none
 export ISSUER=https://samples-javascript.okta.com/oauth2/default
 export SPA_CLIENT_ID=0oapmwm72082GXal14x6
 export USERNAME=george@acme.com
 get_secret prod/okta-sdk-vars/password PASSWORD
+export DEFAULT_TIMEOUT_INTERVAL=90000
 
 if ! yarn test:e2e:samples; then
-  echo "e2e tests failed! Exiting..."
+  echo "e2e-samples tests failed! Exiting..."
   exit ${TEST_FAILURE}
 fi
 
