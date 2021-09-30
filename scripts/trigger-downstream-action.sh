d3b5a88cfd827e8e0640884e6162585b79a3718c
Shuo Wu <wushuo2010@gmail.com>
2021-09-30 14:56:19 -0400

    test

--- scripts/trigger-downstream-action.sh
+++ scripts/trigger-downstream-action.sh
@@ -1,2 +1,3 @@
+echo 'test'
 echo $1
 curl -H "Authorization: token $1" -H "Accept: application/vnd.github.v3+json" -H "Content-Type: application/json" https://api.github.com/repos/okta/samples-js-react/dispatches -d '{ "event_type": "custom_event_type"}'
