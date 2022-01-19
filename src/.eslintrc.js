8b471a2444eccbc78d99fd1be9e3894c50d4eb3b
Jared Perreault <jared.perreault@okta.com>
2022-01-19 16:44:12 +0000

    adds import/no-extraneous-dependencies eslint plugin
    OKTA-460199
    <<<Jenkins Check-In of Tested SHA: a8aa2e78e6ad17c15e0ffb791525b316d91b9422 for eng_productivity_ci_bot_okta@okta.com>>>
    Artifact: okta-react
    Files changed count: 3
    PR Link: "https://github.com/okta/okta-react/pull/194"

--- src/.eslintrc.js
+++ src/.eslintrc.js
@@ -6,6 +6,22 @@ module.exports = {
   },
   extends: [
     'plugin:@typescript-eslint/eslint-recommended',
-    'plugin:@typescript-eslint/recommended'
-  ]
+    'plugin:@typescript-eslint/recommended',
+  ],
+  plugins: [
+    // https://github.com/import-js/eslint-plugin-import#typescript
+    'import',
+  ],
+  rules: {
+    // https://github.com/import-js/eslint-plugin-import/blob/main/docs/rules/no-extraneous-dependencies.md
+    'import/no-extraneous-dependencies': ['error', {
+      'devDependencies': false
+    }]
+  },
+  settings: {
+    // https://github.com/import-js/eslint-plugin-import#typescript
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx']
+    }
+  },
 }
