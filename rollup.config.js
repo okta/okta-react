e163b1a48eb100b1af53cb76dbc772cf740d6f81
Oleksandr Pravosudko <oleksandr.pravosudko@okta.com>
2022-01-28 20:31:38 +0200

    remove unused plugin

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,6 @@ import replace from '@rollup/plugin-replace';
 import cleanup from 'rollup-plugin-cleanup';
 import { terser } from 'rollup-plugin-terser';
 import typescript from 'rollup-plugin-typescript2';
-import json from "@rollup/plugin-json";
 import pkg from "./package.json";
 
 require('./env'); // set variables in process.env
@@ -42,8 +41,7 @@ const commonPlugins = [
   cleanup({ 
     extensions,
     comments: 'none'
-  }),
-  json(),
+  })
 ];
 
 export default [
@@ -61,7 +59,7 @@ export default [
         ],
         extensions
       }),
-      terser(),
+      terser()
     ],
     output: {
       format: 'umd',
