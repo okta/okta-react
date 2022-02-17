418e42aa759f4f7462d889ecc8e4814d1b9d71e2
Shuo Wu <wushuo2010@gmail.com>
2022-02-17 18:01:25 -0500

    use vite for okta-hosted-login sample

---
+++ samples/okta-hosted-login/vite.config.js
@@ -0,0 +1,36 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+const envModule = require('./env')();
+
+envModule.setEnvironmentVarsFromTestEnv(); // Set environment variables from "testenv" file
+
+process.env.CLIENT_ID = process.env.SPA_CLIENT_ID || process.env.CLIENT_ID;
+process.env.OKTA_TESTING_DISABLEHTTPSCHECK = process.env.OKTA_TESTING_DISABLEHTTPSCHECK || false;
+process.env.USE_INTERACTION_CODE = process.env.USE_INTERACTION_CODE || false;
+
+const env = {};
+
+// List of environment variables made available to the app
+[
+  'ISSUER',
+  'CLIENT_ID',
+  'OKTA_TESTING_DISABLEHTTPSCHECK',
+  'USE_INTERACTION_CODE',
+].forEach((key) => {
+  if (!process.env[key]) {
+    throw new Error(`Environment variable ${key} must be set. See README.md`);
+  }
+  env[key] = process.env[key];
+});
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  define: {
+    'process.env': env
+  },
+  server: {
+    port: 8080
+  }
+})
