649bd9bf699b38de80beb8c0b8dfb8158e4110b6
Jared Perreault <jared.perreault@okta.com>
2022-04-28 12:35:31 -0400

    react 18

--- generator/_templates/samples/config/new/package.json.t
+++ generator/_templates/samples/config/new/package.json.t
@@ -15,8 +15,8 @@ force: true
     "preview": "vite preview"
   },
   "dependencies": {
-    "react": "^17.0.2",
-    "react-dom": "^17.0.2",
+    "react": "^18.0.0",
+    "react-dom": "^18.0.0",
 <% if (reactRouterDomVersion !== 'false') { -%>
     "react-router-dom": "<%= reactRouterDomVersion %>",
 <% } -%>
--- samples/routing/reach-router/src/index.tsx
+++ samples/routing/reach-router/src/index.tsx
@@ -11,14 +11,17 @@
  */
 
 import React from 'react';
-import ReactDOM from 'react-dom';
+// import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+createRoot(document.getElementById('root')).render(<App />);
