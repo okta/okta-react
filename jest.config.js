fae8421c1cf41ce0db99a55a8774164ec0fa0bea
Jared Perreault <jared.perreault@okta.com>
2022-09-15 21:25:31 -0400

    fixes jest with dev-7

--- jest.config.js
+++ jest.config.js
@@ -25,7 +25,7 @@ module.exports = {
   ],
   restoreMocks: true,
   moduleNameMapper: {
-    '^@okta/okta-auth-js$': '<rootDir>/node_modules/@okta/okta-auth-js/dist/okta-auth-js.umd.js',
+    // '^@okta/okta-auth-js$': '<rootDir>/node_modules/@okta/okta-auth-js/dist/okta-auth-js.umd.js',
     // avoid react conflict in yarn workspace
     '^react$': '<rootDir>/node_modules/react',
     '^react-dom$': '<rootDir>/node_modules/react-dom',
