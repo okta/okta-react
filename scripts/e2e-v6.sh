b11e716b8b646cca76e8bdcf6721e4da91d07948
Jared Perreault <jared.perreault@okta.com>
2022-09-17 19:46:24 -0400

    adds e2e test against v6

---
+++ scripts/e2e-v6.sh
@@ -0,0 +1,32 @@
+#!/bin/bash -x
+
+source ${OKTA_HOME}/${REPO}/scripts/setup.sh
+
+setup_service java 1.8.222
+setup_service google-chrome-stable 89.0.4389.72-1
+
+export CI=true
+export TEST_SUITE_TYPE="junit"
+export TEST_RESULT_FILE_DIR="${REPO}/test-reports/e2e"
+
+export ISSUER=https://samples-javascript.okta.com/oauth2/default
+export CLIENT_ID=0oapmwm72082GXal14x6
+export SPA_CLIENT_ID=0oapmwm72082GXal14x6
+export USERNAME=george@acme.com
+get_secret prod/okta-sdk-vars/password PASSWORD
+
+if ! yarn workspace @okta/test.app.test-harness-app add @okta/okta-auth-js@^6; then
+  echo "auth-js v6.x could not be installed"
+  exit ${FAILED_SETUP}
+fi
+
+# only run the harness sample app tests
+export SAMPLE_NAME=harness
+if ! yarn test:e2e; then
+  echo "e2e tests failed! Exiting..."
+  exit ${TEST_FAILURE}
+fi
+
+echo ${TEST_SUITE_TYPE} > ${TEST_SUITE_TYPE_FILE}
+echo ${TEST_RESULT_FILE_DIR} > ${TEST_RESULT_FILE_DIR_FILE}
+exit ${PUBLISH_TYPE_AND_RESULT_DIR}
