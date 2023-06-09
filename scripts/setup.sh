02a3954d5d11b5786b6b173394bcbf5c880a4982
denys.oblohin <denys.oblohin@okta.com>
2023-06-09 15:56:47 +0300

    wip . . .

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,9 +1,13 @@
 #!/bin/bash -xe
 
+DIR=$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)
+source $DIR/utils/local.sh
+source $DIR/utils/siw-platform-scripts.sh
+
 # Can be used to run a canary build against a beta AuthJS version that has been published to artifactory.
 # This is available from the "downstream artifact" menu on any okta-auth-js build in Bacon.
 # DO NOT MERGE ANY CHANGES TO THIS LINE!!
-export AUTHJS_VERSION=""
+export AUTHJS_VERSION="7.4.0-g64ea6df"
 
 # Add yarn to the $PATH so npm cli commands do not fail
 export PATH="${PATH}:$(yarn global bin)"
@@ -17,32 +21,17 @@ setup_service yarn 1.21.1 /etc/pki/tls/certs/ca-bundle.crt
 
 cd ${OKTA_HOME}/${REPO}
 
-# undo permissions change on scripts/publish.sh
-git checkout -- scripts
+if [ -n "${TEST_SUITE_ID}" ]; then
+  # undo permissions change on scripts/publish.sh
+  git checkout -- scripts
 
-# ensure we're in a branch on the correct sha
-git checkout $BRANCH
-git reset --hard $SHA
+  # ensure we're in a branch on the correct sha
+  git checkout $BRANCH
+  git reset --hard $SHA
 
-git config --global user.email "oktauploader@okta.com"
-git config --global user.name "oktauploader-okta"
-
-#!/bin/bash
-YARN_REGISTRY=https://registry.yarnpkg.com
-OKTA_REGISTRY=${ARTIFACTORY_URL}/api/npm/npm-okta-master
-
-# Yarn does not utilize the npmrc/yarnrc registry configuration
-# if a lockfile is present. This results in `yarn install` problems
-# for private registries. Until yarn@2.0.0 is released, this is our current
-# workaround.
-#
-# Related issues:
-#  - https://github.com/yarnpkg/yarn/issues/5892
-#  - https://github.com/yarnpkg/yarn/issues/3330
-
-# Replace yarn registry with Okta's
-echo "Replacing $YARN_REGISTRY with $OKTA_REGISTRY within yarn.lock files..."
-sed -i "s#${YARN_REGISTRY}#${OKTA_REGISTRY}#" yarn.lock
+  git config --global user.email "oktauploader@okta.com"
+  git config --global user.name "oktauploader-okta"
+fi
 
 # Install dependencies but do not build
 if ! yarn install --frozen-lockfile --ignore-scripts; then
@@ -50,31 +39,21 @@ if ! yarn install --frozen-lockfile --ignore-scripts; then
   exit ${FAILED_SETUP}
 fi
 
-# Revert the original change(s)
-echo "Replacing $OKTA_REGISTRY with $YARN_REGISTRY within yarn.lock files..."
-sed -i "s#${OKTA_REGISTRY}#${YARN_REGISTRY}#" yarn.lock
-
 # Install a specific version of auth-js, used by downstream artifact builds
 if [ ! -z "$AUTHJS_VERSION" ]; then
   echo "Installing AUTHJS_VERSION: ${AUTHJS_VERSION}"
-  npm config set strict-ssl false
-
-  AUTHJS_URI=https://artifacts.aue1d.saasure.com/artifactory/npm-topic/@okta/okta-auth-js/-/@okta/okta-auth-js-${AUTHJS_VERSION}.tgz
-  if ! yarn add -DW --ignore-scripts ${AUTHJS_URI}; then
-    echo "AUTHJS_VERSION could not be installed: ${AUTHJS_VERSION}"
-    exit ${FAILED_SETUP}
-  fi
 
   # modifies the package.json of all workspaces depending on @okta/okta-auth-js to point to the upstream version
-  ./scripts/utils/sync-ws-auth-js.sh ${AUTHJS_URI}
-  yarn --ignore-scripts
+  $DIR/utils/sync-ws-auth-js.sh ${AUTHJS_VERSION}
 
-  npm config set strict-ssl true
+  install_siw_platform_scripts
+  install_artifact @okta/okta-auth-js "${AUTHJS_VERSION}"
+
   echo "AUTHJS_VERSION installed: ${AUTHJS_VERSION}"
 
   # verify single version of auth-js is installed
   # NOTE: okta-signin-widget will install it's own version of auth-js, filtered out
-  AUTHJS_INSTALLS=$(find . -type d -path "*/node_modules/@okta/okta-auth-js" -not -path "*/okta-signin-widget/*" | wc -l)
+  AUTHJS_INSTALLS=$(find ${OKTA_HOME}/${REPO} -type d -path "*/node_modules/@okta/okta-auth-js" -not -path "*/okta-signin-widget/*" | wc -l)
   if [ $AUTHJS_INSTALLS -gt 1 ]
   then
     echo "ADDITIONAL AUTH JS INSTALL DETECTED"
--- scripts/utils/sync-ws-auth-js.sh
+++ scripts/utils/sync-ws-auth-js.sh
@@ -4,9 +4,12 @@
 
 workspaces=$(yarn -s workspaces info | jq 'map(..|objects|select(.location).location)[1:] | @sh' | tr -d \'\")
 
+# project directory
+pdir=$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )/../.." &> /dev/null && pwd)
+
 for loc in $workspaces
 do
   echo $loc
-  json=$(cat $loc/package.json |  jq --arg version $@ 'if .dependencies | has("@okta/okta-auth-js") then .dependencies["@okta/okta-auth-js"] = $version else . end') && \
-  echo -E "${json}" > $loc/package.json
+  json=$(cat $pdir/$loc/package.json |  jq --arg version $@ 'if .dependencies | has("@okta/okta-auth-js") then .dependencies["@okta/okta-auth-js"] = $version else . end') && \
+  echo -E "${json}" > "$pdir/$loc/package.json"
 done
