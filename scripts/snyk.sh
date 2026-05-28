7f89fc9ea796292da363410054858a590beda1ee
Jared Perreault <90656038+jaredperreault-okta@users.noreply.github.com>
2026-05-28 15:38:04 -0400

    adds snyk scans (#305)
    OKTA-802789 adds snyk scans

--- scripts/snyk.sh
+++ scripts/snyk.sh
@@ -1,15 +1,7 @@
-#!/bin/bash
+#!/bin/bash -x
 
-# When packages change their dependency tree, we want to run snyk monitor. This
-# sends the updated dependency tree to snyk for monitoring.  We want to do this
-# when a commit has been made to master, not on PRs because the dependency tree
-# is likely still in flux while review is happening.
-#
-# For simplicity, this script just runs snyk monitor against all packages on
-# master commits, it doesn't try to figure out which packages were updated.
+source ${OKTA_HOME}/${REPO}/scripts/setup.sh
 
-if [[ "$TRAVIS_BRANCH" == "master" && "$TRAVIS_PULL_REQUEST" == "false" ]]; then
-  yarn global add snyk
-  snyk auth $SNYK_API_TOKEN
-  echo "snyk monitor --org=$SNYK_ORG_ID"
-fi
+create_log_group "Snyk Scan"
+dependency_scan
+finish_log_group $?
