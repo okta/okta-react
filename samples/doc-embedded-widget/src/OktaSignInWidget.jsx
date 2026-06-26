7a912a9114243bd9cce02548b323a0b81bcb5016
Jared Perreault <jared.perreault@okta.com>
2026-06-26 10:52:24 -0400

    ci fix

--- samples/doc-embedded-widget/src/OktaSignInWidget.jsx
+++ samples/doc-embedded-widget/src/OktaSignInWidget.jsx
@@ -12,7 +12,7 @@
 
 import React, { useEffect, useRef } from 'react';
 import OktaSignIn from '@okta/okta-signin-widget';
-import '@okta/okta-signin-widget/dist/css/okta-sign-in.min.css';
+import '@okta/okta-signin-widget/css/okta-sign-in.min.css';
 import config from './config';
 
 const OktaSignInWidget = ({ onSuccess, onError }) => {
