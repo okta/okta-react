16d9edf71400e4f63bc51e7c532e676646439909
Jared Perreault <jared.perreault@okta.com>
2026-09-23 12:06:32 -0400

    fix

--- samples/custom-login/src/polyfills.js
+++ samples/custom-login/src/polyfills.js
@@ -10,9 +10,11 @@
  * See the License for the specific language governing permissions and limitations under the License.
  */
 
-// polyfill TextEncoder for IE Edge
-import { TextEncoder } from 'text-encoding';
+// Example: for polyfilling TextEncoder for IE Edge
+//
+// import { TextEncoder } from 'text-encoding';
 
-if (typeof window.TextEncoder === 'undefined') {
-  window.TextEncoder = TextEncoder;
-}
+// if (typeof window.TextEncoder === 'undefined') {
+//   window.TextEncoder = TextEncoder;
+// }
+
